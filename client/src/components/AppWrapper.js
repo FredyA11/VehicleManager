3fbf596d281ede3267899866e86fbcb49ec36b64
FredyA11 <36737923+FredyA11@users.noreply.github.com>
2021-05-19 18:30:24 -0500

    Updated documentation, fixed bug with login

--- client/src/components/AppWrapper.js
+++ client/src/components/AppWrapper.js
@@ -1,12 +1,12 @@
 import React from "react"
-import AppNav from "./AppNav"
+import AppNav from "./AppNav" //Navbar and the app container
 import Container from "./Container"
-import {Redirect} from "react-router-dom"
-import '../App.css';
+import {Redirect} from "react-router-dom" //Redirect component 
+import '../App.css'; //Stylesheet
 
-const AppWrapper= (props)=>{
+const AppWrapper= (props)=>{ //Component that containes the main app
 
-    if(!props.authorized){
+    if(!props.authorized){ //If someone tries to enter to this page, it will be redirected to the login page, in order to gain access users must log in to the plattform
         return <Redirect to="/" />
     }
     return(
@@ -17,4 +17,4 @@ const AppWrapper= (props)=>{
     )
 }
 
-export default AppWrapper;
+export default AppWrapper; //Make the component visible
