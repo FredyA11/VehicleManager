9aa3e5c5ec4fdc209bdbe7cb41e97a389a860967
FredyA11 <36737923+FredyA11@users.noreply.github.com>
2021-05-19 19:49:25 -0500

    Update server connection

--- client/src/components/axios.js
+++ client/src/components/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios" //Import axios npm package
 
 const api=axios.create({ //Utility to expose and reuse the axios functions to other components , here are the initial settings for the axios package
-    baseURL:"http://localhost:5000",
+    baseURL:"https://drivvu-app.herokuapp.com/",
     timeout:5000
 
 });
