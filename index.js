31d692bd6ac73b2762e2e3688476fd91ffbcc3e4
FredyA11 <36737923+FredyA11@users.noreply.github.com>
2021-05-19 20:22:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ async function encryptPassword(req,res,next){ //Funcion para encriptar la contra
 
 //Routes
 app.get('*', function (req, res) {
-  const index = path.join(__dirname, 'build', 'index.html');
+  const index = path.join(__dirname, 'client/build', 'index.html');
   res.sendFile(index);
 });
 
